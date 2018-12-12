809861a14b175f7f996c3707fcb29d955b25a3d8
douglasfigsouza <douglasfigsouza@gmail.com>
2018-12-12 00:00:07 -0200

    gerando a lista de produtos

--- spoliersweb/src/Pages/Main/index.js
+++ spoliersweb/src/Pages/Main/index.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import api from '../../Services/api';
 
 export default class Main extends Component{
+    state = {
+        products :[]
+    };
     //executado sempre que o componete for montado na tela
     componentDidMount(){
         this.loadProducts();
@@ -9,9 +12,15 @@ export default class Main extends Component{
 
     loadProducts = async() =>{
         const response = await api.get('/products');
-        console.log(response);
+        this.setState({products: response.data.docs});
     }; 
     render(){
-        return <h1>Hello Douglas</h1>
+     return(
+        <div className="product-list">
+        {this.state.products.map(produto=>(
+            <h2 key={produto._id}>{produto.title}</h2>
+        ))}
+       </div>
+     );
     }
 }
